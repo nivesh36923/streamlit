401ada7a1628c36878bf844b07c85d79c1449926
nivesh36923 <niveshofficia@gmail.com>
2025-03-11 11:30:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+name = st.text_input("Enter your name:")
+age = st.number_input("Enter your age:", min_value=1, max_value=100)
+gender = st.selectbox("Select your gender:", ["Male", "Female", "Other"])
+agree = st.checkbox("I agree to the terms")
